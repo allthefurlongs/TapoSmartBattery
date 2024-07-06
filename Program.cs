9960b69c1f863c156eb7b6cca82c76db86e49938
Matt <neverpointless@gmail.com>
2024-07-06 23:41:56 +0100

    Add mutext to avoid running the app more than once

--- Program.cs
+++ Program.cs
@@ -5,9 +5,19 @@ namespace TapoSmartBattery
         [STAThread]
         static void Main()
         {
-            ApplicationConfiguration.Initialize();
-            TapoSmartBatteryForm mainForm = new TapoSmartBatteryForm();
-            Application.Run(mainForm);
+
+            using (Mutex mutex = new Mutex(false, "Global\\TapoSmartBattery-57234ac3-d875-4df5-9a5e-f6ad8b404138"))
+            {
+                if (!mutex.WaitOne(0, false))
+                {
+                    MessageBox.Show("Another instance of TapoSmartBattery is already running", "TapoSmartBattery", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    return;
+                }
+
+                ApplicationConfiguration.Initialize();
+                TapoSmartBatteryForm mainForm = new TapoSmartBatteryForm();
+                Application.Run(mainForm);
+            }
         }
     }
 }
